c8198b57def8c0a9e6c0a5cd52e2d4241fe4f1b9
Meraj al Maksud <btbmerajm@barytech.com>
2024-06-07 12:02:48 +0600

    angular practice first app
    https://v17.angular.io/tutorial/first-app

---
+++ angular-practice/first-app/src/app/home/home.component.ts
@@ -0,0 +1,55 @@
+import { Component, inject } from '@angular/core';
+import { Housinglocation } from '../housinglocation';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingService } from '../housing.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    HousingLocationComponent,
+  ],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" #filter />
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      @for (housingLocation of filteredLocationList; track housingLocation.id) {
+        <app-housing-location
+          [housingLocation]="housingLocation"></app-housing-location>
+      } @empty {
+        Empty list of housing 
+      }
+    </section>
+  `,
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocationList: Housinglocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  filteredLocationList: Housinglocation[] = [];
+
+  constructor() {
+    this.housingService.getAllHousingLocations().then((housingLocationList: Housinglocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    })
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+}
